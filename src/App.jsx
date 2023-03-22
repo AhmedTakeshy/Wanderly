8a915856bb7fba92739aaf75c848f64aa2cab933
Ahmed Abdelsamie <Ahmedtakeshy7@gmail.com>
2023-03-22 22:01:53 +0300

    Feat: Added all missing components and assets

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import Destinations from "./components/Destinations";
+import Booking from "./components/Booking";
+import Selects from "./components/selects/Selects";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Banner />
+      <Destinations />
+      <Booking />
+      <Selects />
+      <Carousel />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
